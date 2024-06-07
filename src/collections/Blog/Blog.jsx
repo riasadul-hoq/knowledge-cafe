6a6902f5f2ba60eeed9c8c39d0f8e63356a0bf48
Riasadul Hoq <shaon777@gmail.com>
2024-06-07 12:50:22 +0100

    v2 Blogs have been published

--- src/collections/Blog/Blog.jsx
+++ src/collections/Blog/Blog.jsx
@@ -1,8 +1,36 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
+  const {
+    id,
+    cover,
+    title,
+    author_img,
+    author,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
   console.log(blog);
-  return <div></div>;
+  return (
+    <div className="border-solid border-4 border-red-600 m-5 p-5">
+      <img src={cover} alt={title}></img>
+      <div className="flex justify-between">
+        <div>
+          <img src={author_img} alt={author}></img>
+          <p>{author}</p>
+          <span>{posted_date}</span>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+
+      <h1 className="font-bold text-4xl custom-text-color">{title}</h1>
+      <span>{hashtags}</span>
+      <button>Mark as read</button>
+    </div>
+  );
 };
 
 Blog.propTypes = {
